5a11059187e52febfafa6167bc5f21aa111706c2
warner3w <48308800+warner3w@users.noreply.github.com>
2019-03-07 14:41:46 +0800

    Add files via upload

---
+++ wftest/wftest/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace wftest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            string str = "X-OpenAM-Username: demo," + "X-OpenAM-Password: changeit," + "Content-Type: application/json";
+                       ;
+            this.label1.Text = PostUrl("https://openam.shinetechinfo.com:8443/openam/json/authenticate", str);
+        }
+        public static string PostUrl(string url, string postData)
+        {
+            string result = "";
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+            req.Method = "POST";
+
+            req.Timeout = 800;//设置请求超时时间，单位为毫秒
+
+            req.ContentType = "application/json";
+
+            byte[] data = Encoding.UTF8.GetBytes(postData);
+
+            req.ContentLength = data.Length;
+
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+
+                reqStream.Close();
+            }
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            Stream stream = resp.GetResponseStream();
+
+            //获取响应内容
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        } 
+    }
+}
